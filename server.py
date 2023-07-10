227b166241d6088d58eb25a55ebfcb3e0a5f22c5
RichardTCrocker <richardtcrocker@gmail.com>
2023-07-10 10:43:18 -0400

    added route

--- server.py
+++ server.py
@@ -3,13 +3,14 @@
 from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
-# @app.route('/')          # The "@" decorator associates this route with the function immediately following
-# def hello_world():
-#     return 'Hello World!'  # Return the string 'Hello World!' as a response
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_world():
+    return 'Hello World!'  # Return the string 'Hello World!' as a response
 
 @app.route('/play')
 @app.route('/play/<int:num>')
 @app.route('/play/<int:num>/<color>')
+@app.route('/play/<color>')
 def play(num=3, color='blue'):
     return render_template("index.html", num=num, color=color)
 
